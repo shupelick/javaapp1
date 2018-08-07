dc7b3207fee65969786359ec614a7a8b3ead2470
shupelick <shupelick@gmail.com>
2018-08-07 17:13:58 +0300

    test

--- src/main/java/loop_for.java
+++ src/main/java/loop_for.java
@@ -2,6 +2,7 @@ public class loop_for {
     public static void main(String[] args) {
         for(int i = 0; i <= 100; i++){
             System.out.println(i*i);
+//            iyhiuhui
         }
     }
 }
